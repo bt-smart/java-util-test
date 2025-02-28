1dfc549f320486f74d1a4540b062a2c1ad441fea
HY <realheyu@qq.com>
2025-02-28 15:48:40 +0800

    init

---
+++ test/src/main/java/com/chico/App.java
@@ -0,0 +1,13 @@
+package com.chico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(test.hello());
+    }
+}
---
+++ test/src/main/java/com/chico/test.java
@@ -0,0 +1,17 @@
+package com.chico;
+
+/**
+ * @author HY
+ * @date 2025/2/28
+ */
+public class test {
+
+
+
+
+
+    public static String hello() {
+        return "Hello World!";
+    }
+
+}
